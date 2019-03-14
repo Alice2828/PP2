7a595876353baa02ab6b8b65fe14a8b2b8cc4c7a
Alice2828 <alisalebedeva28@mail.ru>
2019-03-14 20:34:42 +0600

    1

---
+++ Week6/Snake/Snake/GameState.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+      class GameState
+    {
+        Worm worm = new Worm('O');
+        Wall wall = new Wall('#');
+        Food food = new Food('@');
+        int level = 1;
+       
+        public GameState()
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(40, 40);
+            Console.SetBufferSize(40, 40);
+        }
+
+        public void PressedKey(ConsoleKeyInfo consoleKeyInfo)
+        {
+            switch (consoleKeyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    worm.Move(0, -1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    worm.Move(0, 1);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    worm.Move(-1, 0);
+                    break;
+                case ConsoleKey.RightArrow:
+                    worm.Move(1, 0);
+                    break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    Console.SetCursorPosition(20, 30);
+                    Console.WriteLine("Exit?");
+                    Environment.Exit(0); 
+                    break;
+                case ConsoleKey.F2:
+                    Console.Clear();
+                    worm.body.Clear();
+                    worm.body.Add(new Point { X = 20, Y = 20 });
+                    wall.LoadLevel(2);
+                    food.Generate(wall.body, worm.body);
+                    level = 2;
+                    break;
+                case ConsoleKey.F3:
+                    Console.Clear();
+                    worm.body.Clear();
+                    worm.body.Add(new Point { X = 20, Y = 20 });
+                    wall.LoadLevel(3);
+                    food.Generate(wall.body, worm.body);
+                    level = 3;
+                    break;
+            }
+
+            CheckColision();
+            CheckWallColision();
+            CheckSelfCollision();   
+
+        }
+
+        void CheckColision()
+        {
+            if (worm.CheckIntersection(food.body) == true)
+            {
+                worm.Eat(food.body);
+                food.Generate(worm.body,wall.body);
+                
+            }
+
+        }
+        void CheckWallColision()
+        {
+            if (wall.CheckIntersection(worm.body) == true)
+            {
+                worm.Clear();
+                food.Clear();
+                wall.Clear();
+                Console.WriteLine("     ");
+                Console.WriteLine("Game over");
+                Environment.Exit(0);
+            }
+
+        }
+        void CheckSelfCollision()
+        {
+            if (worm.body.Count>1 && worm.CheckIntersection2() == true)
+            {
+                worm.Clear();
+                food.Clear();
+                wall.Clear();
+                Console.Clear();
+               
+                Console.WriteLine("Game over");
+                Environment.Exit(0);
+
+            }
+        }
+
+
+
+        public void Draw()
+        {
+            worm.Draw();
+            wall.Draw();
+            if (food.body.Count==0)
+            {
+                food.Generate(worm.body, wall.body);
+            }
+            food.Draw();       
+        }
+        public int Score()
+        {
+            return worm.body.Count;
+        }
+        public int Level()
+        {
+            return level;
+        }
+
+    }
+}
---
+++ Week6/Snake/Snake/Wall.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Snake
+{
+    class Wall : GameObject
+    {
+        public Wall(char sign) : base(sign)
+        {
+            LoadLevel(1);
+        }
+
+        public void LoadLevel(int level)
+        {
+            
+            string name = string.Format(@"C:\Users\ASUS\Desktop\PP2\Week6\Snake\Snake\Levels\Level{0}.txt", level);
+           
+            using (StreamReader reader = new StreamReader(name))
+            {
+                int r = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    for (int c = 0; c < line.Length; ++c)
+                    {
+                        if (line[c] == '#')
+                        {
+                            body.Add(new Point { X = c, Y = r });
+                        }
+                    }
+
+                    r++;
+                }
+            }
+           
+        }
+        public bool CheckIntersection(List<Point> otherBody)
+        {
+            bool res = false;
+
+            foreach (Point point in otherBody)
+            {
+                foreach (Point k in body)
+                    if (point.X == k.X &&
+                    point.Y == k.Y)
+                {
+                    res = true;
+                    break;
+                }
+
+                if (res == true)
+                {
+                    break;
+                }
+            }
+
+            return res;
+        }
+       
+
+    }
+}
